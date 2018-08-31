760db19e236fd4baf2b7786dc2c2ea4e0e7bf3cb
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-08-31 08:41:20 +0530

    Setted up request for fetching repos

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,38 +2,47 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-userProfileURL = (user) => {
-   return `https://api.github.com/users/${user}?client_id=a1dc8059a2fe281dcb31&client_secret=ce7948563dc52a43c157bbd9a7f28157541bf0fb`
-}
-
 router.post('/', (request, response) => {
-   axios.get(userProfileURL(request.body.name))
-   .then((res) => {
-      console.log(res.data);
-      if (typeof res.data === 'object') {
-         response.render('index', {
-            data: {
-               loginName: res.data.login,
-               email: res.data.email,
-               avatarUrl: res.data.avatar_url,
-               profileUrl: res.data.html_url,
-               name: res.data.name,
-               company: res.data.company,
-               blog: res.data.blog,
-               location: res.data.location,
-               bio: res.data.bio,
-               repos: res.data.public_repos,
-               followers: res.data.followers,
-               following: res.data.following
-            },
-            isVisible: true,
-            type: 'success',
-            showSuccessModal: true,
-            showErrorModal: false
-         });
-      }
+   axios.all([
+      axios.get(`https://api.github.com/users/${request.body.name}?client_id=a1dc8059a2fe281dcb31&client_secret=ce7948563dc52a43c157bbd9a7f28157541bf0fb`),
+      axios.get(`https://api.github.com/users/${request.body.name}/repos?client_id=a1dc8059a2fe281dcb31&client_secret=ce7948563dc52a43c157bbd9a7f28157541bf0fb`)
+   ])
+   .then(axios.spread((profileRes, reposRes) => {
+      const res = profileRes;
+      const repos = reposRes.data.map(repo => {
+         return {
+            name: repo.name,
+            url: repo.html_url,
+            description: repo.description,
+            language: repo.language,
+            forks_count: repo.forks_count,
+            stargazers_count: repo.stargazers_count,
+            watchers_count: repo.watchers_count
+         }
+      });
 
-   })
+      response.render('index', { 
+         repos: repos,
+         data: {
+            loginName: res.data.login,
+            email: res.data.email,
+            avatarUrl: res.data.avatar_url,
+            profileUrl: res.data.html_url,
+            name: res.data.name,
+            company: res.data.company,
+            blog: res.data.blog,
+            location: res.data.location,
+            bio: res.data.bio,
+            repos: res.data.public_repos,
+            followers: res.data.followers,
+            following: res.data.following
+         },
+         isVisible: true,
+         type: 'success',
+         showSuccessModal: true,
+         showErrorModal: false
+      });
+   }))
    .catch((e) => {
       response.render('index', {
          type: 'Error',
