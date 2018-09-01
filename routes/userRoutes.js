e055d4c445c5b6f69a1e2a5ef0a938ef6e144c23
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-09-02 02:49:54 +0530

    Separated response filtering and rendering into small chunks

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,48 +8,14 @@ router.post('/', (request, response) => {
    axios.all([
       axios.get(`https://api.github.com/users/${request.body.name}?client_id=a1dc8059a2fe281dcb31&client_secret=ce7948563dc52a43c157bbd9a7f28157541bf0fb`),
       axios.get(`https://api.github.com/users/${request.body.name}/repos?client_id=a1dc8059a2fe281dcb31&client_secret=ce7948563dc52a43c157bbd9a7f28157541bf0fb`),
-      axios.get(`https://api.github.com/users/${request.body.name}/followers?client_id=a1dc8059a2fe281dcb31&client_secret=ce7948563dc52a43c157bbd9a7f28157541bf0fb`)
+      axios.get(`https://api.github.com/users/${request.body.name}/followers?client_id=a1dc8059a2fe281dcb31&client_secret=ce7948563dc52a43c157bbd9a7f28157541bf0fb`),
+      axios.get(`https://api.github.com/users/${request.body.name}/following?client_id=a1dc8059a2fe281dcb31&client_secret=ce7948563dc52a43c157bbd9a7f28157541bf0fb`)
    ])
    .then(axios.spread((profileRes, reposRes, followersRes) => {
-      const res = profileRes;
-      const repos = reposRes.data.map(repo => {
-         return {
-            name: repo.name,
-            url: repo.html_url,
-            description: repo.description,
-            language: repo.language,
-            forks_count: repo.forks_count,
-            stargazers_count: repo.stargazers_count,
-            watchers_count: repo.watchers_count
-         }
-      });
-      
-      const followers = followersRes.data.map(follower => {
-         return {
-            login: follower.login,
-            avatar_url: follower.avatar_url,
-            html_url: follower.html_url
-         }
-      });
-      console.log(followers);
-
       response.render('index', { 
-         repos: repos,
-         followers: followers,
-         data: {
-            loginName: res.data.login,
-            email: res.data.email,
-            avatarUrl: res.data.avatar_url,
-            profileUrl: res.data.html_url,
-            name: res.data.name,
-            company: res.data.company,
-            blog: res.data.blog,
-            location: res.data.location,
-            bio: res.data.bio,
-            repos: res.data.public_repos,
-            followers: res.data.followers,
-            following: res.data.following
-         },
+         repos: filterReposData(reposRes),
+         followers: filterFollowersData(followersRes),
+         profile: returnProfileProps(profileRes),
          isVisible: true,
          type: 'success',
          showSuccessModal: true,
@@ -69,5 +35,46 @@ router.post('/', (request, response) => {
       console.log(e);
    });
 });
+
+returnProfileProps = (profile) => {
+   return  {
+      loginName: profile.data.login,
+      email: profile.data.email,
+      avatarUrl: profile.data.avatar_url,
+      profileUrl: profile.data.html_url,
+      name: profile.data.name,
+      company: profile.data.company,
+      blog: profile.data.blog,
+      location: profile.data.location,
+      bio: profile.data.bio,
+      repos: profile.data.public_repos,
+      followers: profile.data.followers,
+      following: profile.data.following
+   }
+}
+
+filterReposData = (repos) => {
+   return repos.data.map(repo => {
+      return {
+         name: repo.name,
+         url: repo.html_url,
+         description: repo.description,
+         language: repo.language,
+         forks_count: repo.forks_count,
+         stargazers_count: repo.stargazers_count,
+         watchers_count: repo.watchers_count
+      }
+   });
+}
+
+filterFollowersData = (followers) => {
+   return followers.data.map(follower => {
+      return {
+         login: follower.login,
+         avatar_url: follower.avatar_url,
+         html_url: follower.html_url
+      }
+   });
+}
 
 module.exports = router;
